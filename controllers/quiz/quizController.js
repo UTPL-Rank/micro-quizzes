e363664e190140deeaf29c52b45a76e7dd784456
Jorge Hurtado <jahurtadod@outlook.com>
2023-02-09 17:09:37 -0500

    Update quizController.js

--- controllers/quiz/quizController.js
+++ controllers/quiz/quizController.js
@@ -19,7 +19,7 @@ exports.quiz_get_byQuantity = async (req, res) => {
   let number = Number(quantity);
   let lengthArray = number;
   var arrayQuestions = [];
-  let array = generateRandomArray(13);
+  let array = generateRandomArray(50);
   try {
     for (let i = 0; i < lengthArray; i++) {
       let index = array[i];
